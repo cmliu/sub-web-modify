47613baf050566c5e081805f759aa3203e0d4e19
zyh552116145 <137603573+zyh552116145@users.noreply.github.com>
2025-07-21 15:47:47 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -796,39 +796,39 @@ export default {
         devid: "",
         interval: "",
         emoji: true,
-        nodeList: false,
-        extraset: false,
-        tls13: false,
-        udp: false,
-        xudp: false,
-        tfo: false,
-        sort: false,
+        nodeList: true,
+        extraset: true,
+        tls13: true,
+        udp: true,
+        xudp: true,
+        tfo: true,
+        sort: true,
         expand: true,
-        scv: false,
-        fdn: false,
-        appendType: false,
-        insert: false, // 是否插入默认订阅的节点，对应配置项 insert_url
+        scv: true,
+        fdn: true,
+        appendType: true,
+        insert: true, // 是否插入默认订阅的节点，对应配置项 insert_url
         new_name: true, // 是否使用 Clash 新字段
         tpl: {
           surge: {
-            doh: false // dns 查询是否使用 DoH
+            doh: true // dns 查询是否使用 DoH
           },
           clash: {
-            doh: false
+            doh: true
           },
           singbox: {
-            ipv6: false
+            ipv6: true
           }
         }
       },
-      loading1: false,
-      loading2: false,
-      loading3: false,
+      loading1: true,
+      loading2: true,
+      loading3: true,
       customSubUrl: "",
       customShortSubUrl: "",
-      dialogUploadConfigVisible: false,
+      dialogUploadConfigVisible: true,
       loadConfig: "",
-      dialogLoadConfigVisible: false,
+      dialogLoadConfigVisible: true,
       uploadFilter: "",
       uploadScript: "",
       uploadConfig: "",
