b1d4f3408d81e6379c9bf569fd7c4a464012f0e3
xytxg <115837765+xytxg@users.noreply.github.com>
2025-11-05 16:31:06 +0000

    简短有意义的提交说明

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -414,22 +414,12 @@ export default {
           "suo.yt": "https://suo.yt/short",
         },
         customBackend: {
-          "CM负载均衡后端【vless reality+hy1+hy2】": "https://subapi.cmliussss.net",
-          "CM应急备用后端【vless reality+hy1+hy2】": "https://subapi.fxxk.dedyn.io",
-          "肥羊增强型后端【vless reality+hy1+hy2】": "https://url.v1.mk",
-          "肥羊备用后端【vless reality+hy1+hy2】": "https://sub.d1.mk",
-          nameless13提供: "https://www.nameless13.com",
-          subconverter作者提供: "https://sub.xeton.dev",
-          "sub-web作者提供": "https://api.wcc.best",
+          "㗽翳自建后端": "https://subapi.990032.xyz",
+          "炊烟自建后端": "https://subapi.010004.xyz",
         },
         backendOptions: [
-          { value: "https://subapi.cmliussss.net" },
-          { value: "https://subapi.fxxk.dedyn.io" },
-          { value: "https://url.v1.mk" },
-          { value: "https://sub.d1.mk" },
-          { value: "https://www.nameless13.com" },
-          { value: "https://sub.xeton.dev" },
-          { value: "https://api.wcc.best" },
+          { value: "https://subapi.990032.xyz" },
+          { value: "https://subapi.010004.xyz" },
         ],
         remoteConfig: [
           {
